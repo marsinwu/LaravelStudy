ce75c0b40325bf5a2c9cdddaf9b9105639f18b19
marcinwuu <w.marcin@yahoo.co.uk>
2017-07-08 11:38:26 +0100

    Added post model and implemented functionality that allows to work with that mode.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+/*Importing Post model*/
+use App\Post;
 
 use App\Http\Requests;
 
@@ -16,10 +18,16 @@ class PostController extends Controller
      */
     public function index()
     {
+        /*Getting all the posts from the database */
+        $posts = Post::all();
+        $deletedPosts = Post::onlyTrashed()->get();
+        /* Array that is going to be passed to the view */
         $data = array(
             'pageTitle' => 'Posts',
+            'postData' => $posts,
+            'trashedData' => $deletedPosts
             //'postData' => DB::select('select id, title from posts')
-            'postData' => DB::table('posts')->select('id', 'title', 'author', 'created_at')->orderBy('id', 'desc')->get()
+            //'postData' => DB::table('posts')->select('id', 'title', 'author', 'created_at')->orderBy('id', 'desc')->get()
         );
 
         return view('posts/show_post_list')->with($data);
@@ -32,6 +40,7 @@ class PostController extends Controller
      */
     public function create()
     {
+        /* Array that is going to be passed to the view */
         $data = array('pageTitle' => 'New post');
         return view('posts/add_post')->with($data);
 
@@ -45,24 +54,40 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        /* Array that is going to be passed to the view */
         $data = array(
             'pageTitle' => 'Add new post',
             'postData' => ''
         );
 
-        if (isset($_POST['post'])) {
-            $post = $_POST['post'];
+        /*Checking if there is post*/
+        if (isset($_POST['post']) && $_POST != null) {
+
+            /*$post = $_POST['post'];
             //$data['postData'] = DB::insert('insert into posts(title,body,author, created_at) values(?,?,?,?)', [$post['title'], $post['body'],$post['author'],date("Y-m-d H:i:s") ]);
             $data['postData'] = DB::table('posts')->insert([
                 'title' => $post['title'],
                 'body' => $post['body'],
                 'author' => $post['author'],
+                'img' => $post['img'],
                 'created_at' => date("Y-m-d H:i:s")
-            ]);
+            ]);*/
+
+            /*Creating new entry to the database vie Post model */
+            $postInput = $_POST['post'];
+            $post = new Post;
+            $post->title = $postInput['title'];
+            $post->body = $postInput['body'];
+            $post->author = $postInput['author'];
+            $post->img = $postInput['img'];
+            $post->created_at = date("Y-m-d H:i:s");
+            /* Post has to be committed(saved) to the databse */
+            $post->save();
 
             return redirect('/posts')->with($data);
 
         } else {
+            /*If for some reason there is problem with the $_POST data user is redirected here */
             return redirect('/posts/new')->with($data);
         }
 
@@ -80,14 +105,17 @@ class PostController extends Controller
      */
     public function show($id)
     {
+
+        /*Array with the data that will be passed to the view */
         $data = array(
             'id' => $id,
             'pageTitle' => 'Post details',
+            //'posts'=>Post::where('id',$id)->take(1)->get()
+            'post' => Post::find($id)
             //'postData' => DB::select('select * from posts where id = ?',[$id])
-            'postData' => DB::table('posts')->where('id', '=', $id)->get()
+            //'postData' => DB::table('posts')->where('id', '=', $id)->get()
         );
         //$postData = DB::select('select * from posts where id = ?',[$id]);
-
 
         return view('posts/show_post')->with($data);
     }
@@ -113,6 +141,7 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
+        /*Array with the data that will be passed to the view */
         $data = array(
             'id' => $id,
             'pageTitle' => 'Post details',
@@ -128,7 +157,41 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        DB::table('posts')->where('id', '=', $id)->delete();
+        /*Destroying entry from database */
+//        DB::table('posts')->where('id', '=', $id)->delete();
+        //$post = Post::find($id);
+        Post::destroy($id);
+        return redirect('/posts');
+    }
+
+    /**
+     * Soft deletion of the post
+     *
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function softdestroy($id)
+    {
+        Post::find($id)->delete();
+        return redirect('/posts');
+    }
+
+    public function destroySandbox($id)
+    {
+
+        Post::onlyTrashed()->where('id', $id)->forceDelete();
+        return redirect('/posts');
+    }
+
+
+    /** Method that restores item from the sandbox to the normal list
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function removeFromTrashAndAddToTheGeneralList($id)
+    {
+
+        Post::onlyTrashed()->where('id', $id)->restore();
         return redirect('/posts');
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,17 +11,20 @@
 |
 */
 
+use App\Post;
 
-/* This is the home page route */
+/* This is the home page route that goes to the home page */
 Route::get('/', function () {
+    /*Array with the data that will be passed to the view */
     $data = array(
         'pageTitle' => 'Laravels'
     );
     return view('welcome')->with($data);
 });
 
-/* This is the about page*/
+/* This is the about page */
 Route::get('/about', function () {
+    /*Array with the data that will be passed to the view */
     $data = array(
         'pageTitle' => 'About Me'
     );
@@ -31,6 +34,7 @@ Route::get('/about', function () {
 
 /* This is the contact me page route */
 Route::get('/contact', function () {
+    /*Array with the data that will be passed to the view */
     $data = array(
         'pageTitle' => 'Contact Me'
     );
@@ -40,25 +44,36 @@ Route::get('/contact', function () {
     return view('contact')->with($data)->with('names', $names);
 });
 
-/* Route that gets all the posts list*/
 
-/* This is the route for adding new post */
+/*GET This is the route for adding new post that goes to the controller (shows the form)*/
 Route::get('/posts', 'PostController@index');
 
-/* Add new post*/
+/*POST Add new post, this is where the data is actually written to the database */
 Route::post('/posts', 'PostController@store');
 
 /* Example route of inserting data into db */
-
 Route::get('/posts/new', 'PostController@create');
 
-
 /* Example of how to get to the post with specific id */
-
 Route::get('/post/{id}', 'PostController@show');
 
 /* Deleting the post */
 Route::get('/post/{id}/delete', 'PostController@destroy');
+
+/* Deleting the post from sandbox */
+Route::get('/post/{id}/permdelete', 'PostController@destroySandbox');
+
+/* Soft deleting the post */
+Route::get('/post/{id}/softdelete', 'PostController@softdestroy');
+
+/* Demo route */
+Route::get('/find', function () {
+    $posts = Post::all();
+});
+
+/* Functionality to restore item from the sandbox */
+Route::get('/post/{id}/restore', 'PostController@removeFromTrashAndAddToTheGeneralList');
+
 
 Route::group(['middlewate' => ['web']], function () {
 
